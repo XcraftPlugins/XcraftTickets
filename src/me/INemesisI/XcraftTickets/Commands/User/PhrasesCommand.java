5e01430477f431f7c43822e973d2f0c263416d51
Nemesis <Nemesis@Benedikt>
2013-03-13 18:44:55 +0100

    added default config.yml to prevent errors at initial startup

---
+++ src/me/INemesisI/XcraftTickets/Commands/User/PhrasesCommand.java
@@ -0,0 +1,49 @@
+package me.INemesisI.XcraftTickets.Commands.User;
+
+import java.util.Map;
+
+import me.INemesisI.XcraftTickets.Commands.Command;
+import me.INemesisI.XcraftTickets.Commands.CommandInfo;
+import me.INemesisI.XcraftTickets.Manager.TicketManager;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+@CommandInfo(name = "phrases",
+		command = "ticket",
+		pattern = "p.*",
+		permission = "XcraftTickets.Phrases",
+		usage = "list|add|remove",
+		desc = "Zeigt alle Ticket-Phrasen")
+public class PhrasesCommand extends Command {
+
+	@Override
+	public boolean execute(TicketManager manager, CommandSender sender, String[] args) {
+		Map<String, String> map = manager.getPhrases();
+		if (args.length < 1)
+			return false;
+		if (args[0].equals("list")) {
+			reply(sender, ChatColor.DARK_AQUA + "Predefined Phrases: ");
+			for (String key : map.keySet()) {
+				reply(sender, ChatColor.DARK_AQUA + key + ": " + ChatColor.DARK_GRAY + map.get(key));
+			}
+		} else if (args[0].equals("add")) {
+			String key = args[1];
+			String msg = "";
+			for (int i = 2; i < args.length; i++) {
+				msg += args[i] + " ";
+			}
+			msg.trim();
+			manager.getPhrases().put(key, msg);
+			reply(sender, "Successuflly added the phrase (" + key + " = " + msg + ")");
+		} else if (args[0].equals("remove")) {
+			String key = args[1];
+			if (manager.getPhrases().remove(key) != null) {
+				reply(sender, "Successuflly removed the phrase");
+			} else {
+				error(sender, "Could not find a phrase with that name");
+			}
+		}
+		return true;
+	}
+}
